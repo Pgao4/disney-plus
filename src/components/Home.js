949ad379a5fe7225a35828316c3b731748b88d23
pgao4 <pgao4@ualberta.ca>
2022-02-02 21:16:45 -0700

    ADD: More types

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,26 +4,64 @@ import styled from 'styled-components';
 import ImgSlider from './ImgSlider';
 import Viewers from './Viewers';
 import Movies from './Movies';
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { setMovies } from '../features/movie/movieSlice'
+import NewDisney from './Newdisney';
+import Originals from './Originals';
+import Trending from './Trending';
+import { selectUserName } from "../features/user/userSlice";
 
 function Home() {
   const dispatch = useDispatch();
+  const userName = useSelector(selectUserName);
+  let recommends = [];
+  let newDisneys = [];
+  let originals = [];
+  let trending = [];
 
-  useEffect(()=>{
-      db.collection("movies").onSnapshot((snapshot)=>{
-          let tempMovies = snapshot.docs.map((doc)=>{
-              return { id: doc.id, ...doc.data() }
-          })
-          dispatch(setMovies(tempMovies));
-      })
-  }, [])
+  useEffect(() => {
+    console.log("hello");
+    db.collection("movies").onSnapshot((snapshot) => {
+      snapshot.docs.map((doc) => {
+        console.log(recommends);
+        switch (doc.data().type) {
+          case "recommend":
+            recommends = [...recommends, { id: doc.id, ...doc.data() }];
+            break;
+
+          case "new":
+            newDisneys = [...newDisneys, { id: doc.id, ...doc.data() }];
+            break;
+
+          case "original":
+            originals = [...originals, { id: doc.id, ...doc.data() }];
+            break;
+
+          case "trending":
+            trending = [...trending, { id: doc.id, ...doc.data() }];
+            break;
+        }
+      });
+
+      dispatch(
+        setMovies({
+          recommend: recommends,
+          newDisney: newDisneys,
+          original: originals,
+          trending: trending,
+        })
+      );
+    });
+  }, [userName]);
 
   return (
       <Container>
         <ImgSlider />
         <Viewers />
         <Movies />
+        <NewDisney />
+        <Originals />
+        <Trending />
       </Container>
   );
 }
