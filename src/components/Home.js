db423cbefcbeb253a7df66632a8743938537680f
pgao4 <pgao4@ualberta.ca>
2022-02-03 22:49:01 -0700

    Fit mobile

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,10 +20,8 @@ function Home() {
   let trending = [];
 
   useEffect(() => {
-    console.log("hello");
     db.collection("movies").onSnapshot((snapshot) => {
       snapshot.docs.map((doc) => {
-        console.log(recommends);
         switch (doc.data().type) {
           case "recommend":
             recommends = [...recommends, { id: doc.id, ...doc.data() }];
